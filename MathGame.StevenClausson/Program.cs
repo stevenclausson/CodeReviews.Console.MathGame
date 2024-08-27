ec855c4af99a69a41c7afe4860a7af98ad47725b
Steven <sclausson@gmail.com>
2024-08-27 11:48:40 -0700

    Initial? Test?

---
+++ MathGame.StevenClausson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MathGame.StevenClausson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finished!");
+        }
+    }
+}
